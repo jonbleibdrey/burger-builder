7f78e57c4eb3d7eb56142b4b9594b623316744b3
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-12-08 09:50:05 -0500

    created photo file , logo file

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = (props) => {
+    return (
+        <div>
+            <img src="../../assets/images/burger-logo.png" alt="">
+        </div>
+    )
+}
+
+export default Logo;
