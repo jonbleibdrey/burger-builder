3b97e0e28c98d16133e2fd2f56cb952ac7244542
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-12-08 10:45:15 -0500

    made the navaigation buttons

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Toolbar.css"
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const Toolbar = (props) => {
     return ( 
@@ -8,7 +9,7 @@ const Toolbar = (props) => {
             <div>MENU</div>
             <Logo/>
             <nav>
-                ...
+                <NavigationItems/>
             </nav>
         </header>
 
