33e05f5c290141582ef95cdd9607bdb140d6e738
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-12-09 11:08:56 -0500

    created a sliding drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../hoc/Aux'
 import './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const Layout = (props) => {
-    return (
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false})
+    }
+
+    render(){
+        return(
         <Aux>
-           <Toolbar/>
-           <SideDrawer/>
-        <main className="Content"> {props.children} </main>
-
-        </Aux>
-    )
-}
-
+            <Toolbar/>
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+            <main className="Content"> {this.props.children} </main>
+         </Aux>
+        )
+    }
+} 
 export default Layout
